20ebbfbba194c7349761f6444d6ec5de2fa5fe6f
Leo Cassarani <me@leocassarani.net>
2015-01-06 14:23:20 +0000

    Keep ready_list in descending order of priority

--- threads/thread.c
+++ threads/thread.c
@@ -70,6 +70,9 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+static bool priority_higher (const struct list_elem *a,
+                             const struct list_elem *b,
+                             void *aux UNUSED);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -237,7 +240,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, priority_higher, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -308,7 +311,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, priority_higher, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -578,6 +581,18 @@ allocate_tid (void)
 
   return tid;
 }
+
+/* Returns true if the thread associated with a has higher
+   priority than the one associated with b, false otherwise. */
+bool
+priority_higher (const struct list_elem *a,
+                 const struct list_elem *b, void *aux UNUSED)
+{
+    struct thread *t_a = list_entry (a, struct thread, elem);
+    struct thread *t_b = list_entry (b, struct thread, elem);
+    return t_a->priority > t_b->priority;
+}
+
 
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */

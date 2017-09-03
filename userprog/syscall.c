1642d05a12eb9174cbd720559667e6c644a61112
Leo Cassarani <me@leocassarani.net>
2017-09-03 23:13:23 +0000

    Implement the halt system call

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,12 +1,15 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
 static void syscall_exit (int);
 static int syscall_write (int, const void *, unsigned);
+
+static uint32_t get_syscall_arg (void *, int);
 
 void
 syscall_init (void) 
@@ -21,16 +24,18 @@ syscall_handler (struct intr_frame *f UNUSED)
 
   switch (syscall_nr)
     {
+    case SYS_HALT:
+      shutdown_power_off ();
     case SYS_EXIT:
       {
-        int status = *((int *) f->esp + 1);
+        int status = get_syscall_arg (f->esp, 1);
         syscall_exit (status);
       }
     case SYS_WRITE:
       {
-        int fd = *((int *) f->esp + 1);
-        const void *buffer = *((void **) f->esp + 2);
-        unsigned size = *((unsigned *) f->esp + 3);
+        int fd = get_syscall_arg (f->esp, 1);
+        const void *buffer = (void *) get_syscall_arg (f->esp, 2);
+        unsigned size = (unsigned) get_syscall_arg (f->esp, 3);
         f->eax = syscall_write (fd, buffer, size);
       }
     }
@@ -50,4 +55,10 @@ syscall_write (int fd, const void *buffer, unsigned size)
     putbuf (buffer, size);
 
   return size;
+}
+
+static uint32_t
+get_syscall_arg (void *esp, int idx)
+{
+  return *((uint32_t *) esp + idx);
 }
